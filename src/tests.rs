bc66a87422031e48f480281ff9623c41a0ba356d
AlvaroMS25 <alvaro2ms5@gmail.com>
2023-03-30 12:29:58 +0200

    test

--- src/tests.rs
+++ src/tests.rs
@@ -1,20 +1,20 @@
 use crate::{BinaryWrite, BinaryRead};
-use std::{fs::{OpenOptions}, io};
+use std::{fs::{OpenOptions}, io::{self}};
 
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
+#[allow(unused)]
 struct Test {
-    f: usize,
-    d: char,
-    g: u64
+    f: u32,
+    d: usize,
+    g: u8
 }
 
 impl Test {
-    fn new() -> Self {
+    fn random() -> Self {
         Self {
-            f: 32,
-            d: 'a',
-            g: 13451234
+            f: rand::random(),
+            d: rand::random(),
+            g: rand::random()
         }
     }
 }
@@ -27,9 +27,10 @@ fn write_file() -> io::Result<()> {
     .truncate(true)
     .open("./test_file.bin")?;
 
-    let s = Test::new();
+    let s = Test::random();
 
-    file.write(&s)?;
+    file.write_binary(&s)?;
+    println!("{s:?}");
 
     Ok(())
 }
@@ -38,11 +39,28 @@ fn write_file() -> io::Result<()> {
 fn read_file() -> io::Result<()> {
     let mut file = OpenOptions::new()
     .read(true)
-    .open("./test_file.bin")?;
+    .open("./test_file2.bin")?;
 
-    println!("Size {}", std::mem::size_of::<Test>());
-    let a = file.read::<Test>()?;
-    println!("Juan: {}", a.g);
+    let a = file.read_binary::<Test>()?;
+    println!("{a:?}");
 
+    Ok(())
+}
+
+fn write_buffer() -> io::Result<(Test, io::Cursor<Vec<u8>>)> {
+    let test = Test::random();
+    let mut buf = Vec::new();
+
+    buf.write_binary(&test)?;
+
+    Ok((test, io::Cursor::new(buf)))
+}
+
+#[test]
+fn read_write_buffer() -> io::Result<()> {
+    let (original, mut buf) = write_buffer()?;
+    let test = buf.read_binary::<Test>()?;
+    
+    assert_eq!(original, test);
     Ok(())
 }
