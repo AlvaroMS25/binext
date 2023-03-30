7868c00b6f16878effd6e4743456fa4d79c8e7e0
AlvaroMS25 <alvaro2ms5@gmail.com>
2023-03-30 11:51:56 +0200

    rename to *_binary

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,15 @@ mod tests;
 use std::{alloc::{alloc, Layout}, io::{self, Write, Read}, mem::size_of, slice};
 
 pub trait BinaryRead {
-    fn read<T>(&mut self) -> io::Result<T>;
+    fn read_binary<T>(&mut self) -> io::Result<T>;
 }
 
 pub trait BinaryWrite {
-    fn write<T>(&mut self, item: &T) -> io::Result<()>;
+    fn write_binary<T>(&mut self, item: &T) -> io::Result<()>;
 }
 
 impl<I: Write> BinaryWrite for I {
-    fn write<T>(&mut self, item: &T) -> io::Result<()> {
+    fn write_binary<T>(&mut self, item: &T) -> io::Result<()> {
         let ptr = item as *const T as *mut u8;
         let buf = unsafe {
             slice::from_raw_parts(ptr, size_of::<T>())
@@ -25,7 +25,7 @@ impl<I: Write> BinaryWrite for I {
 }
 
 impl<I: Read> BinaryRead for I {
-    fn read<T>(&mut self) -> io::Result<T> {
+    fn read_binary<T>(&mut self) -> io::Result<T> {
         Ok(unsafe {
             let ptr = alloc(Layout::new::<T>());
 
